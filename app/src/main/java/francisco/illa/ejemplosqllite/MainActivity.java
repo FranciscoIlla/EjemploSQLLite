be47c6a282d095073f3e3fad35bbac7b2d59b54e
franciscoillacases <francisco.illacases@cieep.com>
2023-11-27 11:25:50 +0100

    Initial commit

---
+++ app/src/main/java/francisco/illa/ejemplosqllite/MainActivity.java
@@ -0,0 +1,42 @@
+package francisco.illa.ejemplosqllite;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import francisco.illa.ejemplosqllite.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+
+}
